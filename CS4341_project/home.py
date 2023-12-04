690077b02b811f5b13b9f52c6afbb3c413d8ecce
Ishayu Das <ishayu.das@gmail.com>
2023-12-03 19:41:44 -0500

    changes v4

--- CS4341_project/home.py
+++ CS4341_project/home.py
@@ -1,11 +1,12 @@
 import webbrowser
 import os
+from flask import Flask, jsonify, redirect, render_template
 
+app = Flask(__name__)
 
 def main():
     file_path = 'CS4341_project\pages\home.html'
     webbrowser.open_new_tab('file://' + os.path.realpath(file_path))  # Opens HTML file in new tab
-
 
 if __name__ == '__main__':
     main()
